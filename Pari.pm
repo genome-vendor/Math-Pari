1619ace124febd3d65179643eb04804b90b3c03d
Ilya Zakharevich <cpan@ilyaz.org>
1998-11-06 15:29:00 -0800

    import Math-Pari 2.001200 from CPAN
    git-cpan-module:   Math-Pari
    git-cpan-version:  2.001200
    git-cpan-authorid: ILYAZ
    git-cpan-file:     authors/id/I/IL/ILYAZ/modules/Math-Pari-2.001200.tar.gz

--- Pari.pm
+++ Pari.pm
@@ -759,7 +759,7 @@ sub AUTOLOAD {
 $initmem = $initmem || 4000000;		# How much memory for the stack
 $initprimes = $initprimes || 500000;	# Calculate primes up to this number
 
-$VERSION = '2.001104';
+$VERSION = '2.001200';
 
 bootstrap Math::Pari;
 
